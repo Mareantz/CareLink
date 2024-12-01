3485f1a4658c1fb58512c57daf92fdc08f4c73d1
Mareantz <marianciotir@gmail.com>
2024-12-01 14:26:45 +0200

    trying to deploy the app on railway

--- HealthcareManagementSystem/HealthcareManagementSystem/Program.cs
+++ HealthcareManagementSystem/HealthcareManagementSystem/Program.cs
@@ -3,6 +3,10 @@ using Application.Utils;
 using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
+builder.Services.AddHealthChecks();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -11,6 +15,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseHealthChecks("/health");
 
 if (app.Environment.IsDevelopment())
 {
