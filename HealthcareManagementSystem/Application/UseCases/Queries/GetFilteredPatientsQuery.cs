4877473bdaf56de889c31d8f04f92cf73fec9d3d
Mareantz <marianciotir@gmail.com>
2024-12-05 17:33:40 +0200

    reverted to localhost deployment & added pagination for patients

---
+++ HealthcareManagementSystem/Application/UseCases/Queries/GetFilteredPatientsQuery.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs;
+using Application.Utils;
+using Domain.Common;
+using MediatR;
+
+namespace Application.UseCases.Queries
+{
+	public class GetFilteredPatientsQuery : IRequest<Result<PagedResult<PatientDto>>>
+	{
+		public int Page { get; set; }
+		public int PageSize { get; set; }
+	}
+}
--- HealthcareManagementSystem/HealthcareManagementSystem/Program.cs
+++ HealthcareManagementSystem/HealthcareManagementSystem/Program.cs
@@ -4,9 +4,8 @@ using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables();
-Console.WriteLine($"Connection String #1: {builder.Configuration.GetConnectionString("DefaultConnection")}");
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://*:{port}");
+//builder.WebHost.UseUrls($"http://*:{port}");
 
 var AllowFrontend = "AllowFrontend";
 
@@ -22,13 +21,7 @@ builder.Services.AddCors(options =>
 
 });
 
-Console.WriteLine("Environment Variables:");
-foreach (var variable in Environment.GetEnvironmentVariables().Keys)
-{
-	Console.WriteLine($"{variable}: {Environment.GetEnvironmentVariable(variable.ToString())}");
-}
 builder.Configuration["ConnectionStrings:DefaultConnection"] = Environment.GetEnvironmentVariable("DefaultConnection");
-Console.WriteLine($"Connection String #2: {builder.Configuration.GetConnectionString("DefaultConnection")}");
 
 
 builder.Services.AddHealthChecks();
