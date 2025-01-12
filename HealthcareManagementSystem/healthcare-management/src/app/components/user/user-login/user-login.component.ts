4ae376ffa3aa7ed29daa1571d1496ce89cd0c8db
RogoMY <mihairogo2004@gmail.com>
2025-01-12 18:00:21 +0200

    refacere login

--- HealthcareManagementSystem/healthcare-management/src/app/components/user/user-login/user-login.component.ts
+++ HealthcareManagementSystem/healthcare-management/src/app/components/user/user-login/user-login.component.ts
@@ -3,10 +3,15 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
